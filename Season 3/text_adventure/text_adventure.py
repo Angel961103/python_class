49ab40840c8a9fcc9f08a83fcaf187cfee44d73a
Angel <koangel961103@gmail.com>
2021-11-20 16:47:52 +0800

    text adventure

--- Season 3/text_adventure/text_adventure.py
+++ Season 3/text_adventure/text_adventure.py
@@ -33,20 +33,55 @@ class Button(Sprite):
         self.label.delete()
         super().delete()
 
+class Player:
+    def __init__(self):
+        self.items : List[Item] = []
+        
+player = Player()
+
+
+
+class Item(Sprite):
+    def on_create(self):
+        self.name = ""
+        self.node = None
+        self.is_visible = False
+
+    def on_left_click(self):
+        player.items.append(self)
+        self.node.remove_item(self)
+        
+
 class Node:
     def __init__(self, name: str, img: str):
-        self.room_name = name
+        self.name = name
         self.background = w.create_sprite(image=img)
         self.background.position = w.center
         self.background.is_visible = False
         self.neighbors: List[Node] = []
         self.buttons: List[Button] = []
         self.messages:List[str] = []
+        self.items: List[Item] = []
         print("node created")
 
     def add_path(self, node: "Node", msg: str):
         self.neighbors.append(node)
         self.messages.append(msg)
+
+    def add_item(self, item: Item):
+        self.items.append(item)
+        item.node = self
+
+    def remove_item(self, item: Item):
+        self.items.remove(item)
+        item.node = None
+        item.is_visible = False
+        for i in range(len(self.neighbors)):
+            node = self.neighbors[i]
+            if isinstance(node, SceretNode):
+                if node.required_item in player.items:
+                    self.create_button(self.messages[i], node)
+
 
     def create_button(self, msg: str, node: 'Node'):
         print(msg)
@@ -58,21 +93,43 @@ class Node:
 
     def show(self):
         self.background.is_visible = True
+        for item in self.items:
+            item.is_visible = True
+        
         for i in range(len(self.neighbors)):
-            self.create_button(self.messages[i], self.neighbors[i])
+            node = self.neighbors[i]
+            if isinstance(node, SceretNode):
+                if node.required_item in player.items:
+                    self.create_button(self.messages[i], node)
+
+            else:
+                self.create_button(self.messages[i], node)
 
     def hide(self):
         self.background.is_visible = False
         for b in self.buttons:
             b.delete()
         self.buttons.clear()
+        for item in self.items:
+            item.is_visible = False
+
+class SceretNode(Node):
+    def __init__(self, name: str, img: str, required_item: Item):
+        super().__init__(name, img)
+        self.required_item = required_item
 
 
+
+key = w.create_sprite(Item, image="key.PNG", scale=0.3, x=600, y=200, layer=5)
 castle = Node(name="Castle", img="Castle.PNG")
 inside_castle = Node(name="inside_castle", img="Castle_inside.PNG")
 field = Node(name="field", img="field.PNG")
+room = SceretNode(name="room", img="room.PNG", required_item=key)
 
 field.add_path(castle, "go back to castle")
+
+inside_castle.add_path(room, "go to the room")
+inside_castle.add_item(key)
 
 castle.add_path(inside_castle, "go inside")
 castle.add_path(field, "go to field")
