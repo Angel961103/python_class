2aed29097520ee2adab5c8ac0295aad4ead1c303
Angel <koangel961103@gmail.com>
2022-01-08 15:55:21 +0800

    tetris

--- Season 3/tetris/tetris.py
+++ Season 3/tetris/tetris.py
@@ -6,20 +6,24 @@ from pycat.sprite import Sprite
 from typing import List
 
 w = Window()
-
-GROUND_Y = 100
+TOP_Y = 600
+GAP = 3
+SCALE = 30
+ROWS = 18
+GROUND_Y = TOP_Y-(ROWS-1)*(GAP+SCALE)
 
 class Cell(Sprite):
     def on_create(self):
         self.color = Color.YELLOW
-        self.scale = 30
+        self.scale = SCALE
         self.x = 600
-        self.y = 600
-        self.y_speed = 30
-        self.x_speed = 35
+        self.y = TOP_Y
+        self.y_speed = SCALE+GAP
+        self.x_speed = SCALE+GAP
         self.time = 0
         self.add_tag("cell")
         self.state = Cell.State.FALL
+        self.move_time = 0.5
 
     class State(Enum):
         FALL = auto()
@@ -41,17 +45,10 @@ class Cell(Sprite):
                 self.x -= self.x_speed
 
     def down(self):
-        if w.is_key_up(KeyCode.S):
-            while not self.is_touching_any_sprite_with_tag("cell"):
-                self.y -= self.y_speed
-                if self.y <= GROUND_Y:
-                    self.lock()
-                self.y = GROUND_Y
-                w.create_sprite(Cell)
-                break
-            self.lock()
-            return True
-        return False
+        if w.is_key_pressed(KeyCode.S):
+            self.move_time = 0.1
+        else:
+            self.move_time = 0.5
 
     def check_ground(self):
         if self.y < GROUND_Y:
@@ -65,23 +62,21 @@ class Cell(Sprite):
     def check_cell(self):
         cells: List[Cell] = self.get_touching_sprites_with_tag("cell")
         if cells:
-            self.y = cells[0].y + self.height + 5
+            self.y = cells[0].y + self.height + GAP
             self.lock()
             w.create_sprite(Cell)
 
     def on_update(self, dt):
-        if self.state == Cell.State.FALL:
+        if self.state is Cell.State.FALL:
             self.time += dt
-            if self.time > 0.5:
+            if self.time > self.move_time:
                 self.y -= self.y_speed
                 self.time = 0
-        self.left()
-        self.right()
-        if self.down():
-            return
-        if self.check_cell():
-            return
-        self.check_ground()
+            self.left()
+            self.right()
+            self.down()
+            self.check_cell()
+            self.check_ground()
             
 
 w.create_sprite(Cell)
