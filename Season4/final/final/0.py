76010931447b84dc8791def417aca3129df77b8d
Angel <chase136851@gmail.com>
2022-06-12 15:37:27 +0800

    final

--- Season4/final/final/0.py
+++ Season4/final/final/0.py
@@ -13,19 +13,13 @@ class Level(Sprite):
         for g in get_levels_entities(f)[0].entities:
             w.create_sprite(x=g.x, y=g.y, scale_x=g.width, scale_y=g.height, tags=g.tags, opacity=55)
 
-DV = 2
-MAX_V = 10
-JUMP_SPEED = 17
-FRCITION = 0.78
-GRAVITY = 1
-
 class Player(Sprite):
-    GRAVITY = -0.9
-    JUMP_VELOCITY = 15
-    FRICTION = 0.9
-    MAX_SPEED = 20
-    ACCELERATION = 1.
-    CLIMB_SPEED = 4
+    GRAVITY = 1.5
+    JUMP_VELOCITY = 17
+    FRICTION = 0.78
+    MAX_SPEED = 10
+    ACCELERATION = 1
+    CLIMB_SPEED = 2
 
     class State(Enum):
         STANDING = auto()
@@ -35,7 +29,7 @@ class Player(Sprite):
         CLIMBING = auto()
 
     def on_create(self):
-        self.image = "png/character_0001.png"
+        self.image = "character_0001.png"
         self.y = 200
         self.x = 100
         self.vx = 0
@@ -45,7 +39,7 @@ class Player(Sprite):
         self.is_on_platform = False
         self.add_tag("player")
         self.layer = 5
-        self.state = Player.State.STANDING
+        self.state = Player.State.FALLING
 
     def is_on_a_ladder(self):
         return bool(self.get_touching_sprites_with_tag('l'))
@@ -58,47 +52,90 @@ class Player(Sprite):
         ladders = self.get_touching_sprites_with_tag('l')
         return self.y < ladders[0].y if ladders else False
 
-    def walk(self):
+    def update_vx(self):
         if w.is_key_pressed(KeyCode.D):
-            self.vx += DV
-            if self.vx > MAX_V:
-                self.vx = MAX_V
-            self.state = Player.State.WALKING
+            self.vx += Player.ACCELERATION
+            if self.vx > Player.MAX_SPEED:
+                self.vx = Player.MAX_SPEED
+            
         if w.is_key_pressed(KeyCode.A):
-            self.vx -= DV
-            if self.vx < -MAX_V:
-                self.vx = -MAX_V
-            self.state = Player.State.WALKING
+            self.vx -= Player.ACCELERATION
+            if self.vx < -Player.MAX_SPEED:
+                self.vx = -Player.MAX_SPEED
+        self.vx *= Player.FRICTION
+        self.x += self.vx
+        if abs(self.vx) < 0.2:
+            self.vx = 0
+
+    def update_vy(self):
+        self.y += self.vy
+        self.vy -= Player.GRAVITY
+
 
     def on_update(self, dt):
 
         if self.state is Player.State.STANDING:
-            if not self.is_touching_any_sprite_with_tag("g"):
-                self.state = Player.State.FALLING
-            self.walk()
+            self.update_vx()
+            if self.vx != 0:
+                self.state = Player.State.WALKING
             if w.is_key_down(KeyCode.W):
-                self.vy = JUMP_SPEED
+                self.vy = Player.JUMP_VELOCITY
                 self.state = Player.State.JUMPING
+            if self.is_on_a_ladder():
+                if w.is_key_pressed(KeyCode.UP):
+                    self.state = Player.State.CLIMBING
             
         elif self.state is Player.State.WALKING:
-            self.x += self.vx
-            self.walk()
-            self.vx *= FRCITION
-            if w.is_key_down(KeyCode.W):
-                self.vy = JUMP_SPEED
+            self.update_vx()
+            if not self.is_touching_any_sprite_with_tag("g"):
+                self.state = Player.State.FALLING
+                self.vy = 0
+            if self.vx == 0:
+                self.state = Player.State.STANDING
+            
+            elif w.is_key_down(KeyCode.W):
+                self.vy = Player.JUMP_VELOCITY
                 self.state = Player.State.JUMPING
+
         elif self.state is Player.State.FALLING:
-            self.y += self.vy
-            self.vy -= GRAVITY
-            g = w.get_sprite_with_tag("g")
+            self.update_vx()
+            self.update_vy()
+            g = self.get_touching_sprites_with_tag("g")
             if g:
-                self.y = g.y + g.height/2 + self.height/2
-                self.state = Player.State.STANDING 
+                ground_y = g[0].y + g[0].height/2 + self.height/2
+                if self.y - self.vy > ground_y:
+                    self.y = ground_y
+                    self.state = Player.State.STANDING
+            # TO DO
+            # else:
+            #     l = self.get_touching_sprites_with_tag("l")
+            #     if l:
+            #         self.y = l[0].y + l[0].height/2 + self.height/2
+            #         self.state = Player.State.STANDING
+
         elif self.state is Player.State.JUMPING:
-            self.y += self.vy
-            self.vy -= GRAVITY
+            self.update_vx()
+            self.update_vy()
             if self.vy < 0:
                 self.state = Player.State.FALLING
+        
+        elif self.state is Player.State.CLIMBING:
+            self.update_vx()
+            if self.is_on_a_ladder():
+                if w.is_key_pressed(KeyCode.UP):
+                    self.y += Player.CLIMB_SPEED
+                    if not self.is_on_a_ladder():
+                        self.state = Player.State.STANDING
+                        self.vy = 0
+                if w.is_key_pressed(KeyCode.DOWN):
+                    self.y -= Player.CLIMB_SPEED
+                    if self.is_touching_any_sprite_with_tag("g"):
+                        self.state = Player.State.STANDING
+                        self.vy = 0
+            elif not self.is_on_a_ladder():
+                self.state = Player.State.FALLING
+                self.vy = 0
+
                 
         # if w.is_key_pressed(KeyCode.D):
         #     self.vx += DV
